393d58a015ee60f403c57d380e64ada404eb3d7c
Srinivas R <srinivaspes@gmail.com>
2025-06-09 16:05:58 +0530

    Create login.component.ts

---
+++ src/app/modules/shared/components/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  mobile = '';
+  otp = '';
+  otpSent = false;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  generateOtp() {
+    this.authService.generateOtp(this.mobile).subscribe(() => {
+      this.otpSent = true;
+    });
+  }
+
+  login() {
+    this.authService.verifyOtp(this.mobile, this.otp).subscribe(valid => {
+      if (valid) {
+        this.authService.onboardUser({ mobile: this.mobile, role: 'resident', name: 'User', apartmentId: 'apt1', isApproved: false })
+          .subscribe(user => {
+            this.router.navigate([user.role === 'admin' ? '/admin' : '/resident']);
+          });
+      }
+    });
+  }
+}

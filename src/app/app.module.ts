4193ec7140c0a10965aed97d4fc87eb1dfa283cb
Srinivas R <srinivaspes@gmail.com>
2025-06-09 16:42:02 +0530

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './modules/shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { reducer } from './core/store/reducers';
+import { AppEffects } from './core/store/effects';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    HttpClientModule,
+    StoreModule.forRoot({ app: reducer }),
+    EffectsModule.forRoot([AppEffects])
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}

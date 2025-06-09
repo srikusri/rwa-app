406846cd9e2a159476d5ee0de3e91ac064af3a87
Srinivas R <srinivaspes@gmail.com>
2025-06-09 16:46:01 +0530

    Create users.js

---
+++ backend/data/users.js
@@ -0,0 +1,6 @@
+let users = [
+  { id: '1', mobile: '1234567890', role: 'admin', name: 'Admin User', apartmentId: 'apt1', isApproved: true },
+  { id: '2', mobile: '0987654321', role: 'resident', name: 'Resident User', apartmentId: 'apt1', isApproved: false }
+];
+
+module.exports = { users };

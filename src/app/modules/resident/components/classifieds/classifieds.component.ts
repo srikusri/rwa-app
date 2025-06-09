0ea87b074863dbb029c620f649ab5b11e284fcf9
Srinivas R <srinivaspes@gmail.com>
2025-06-09 15:42:36 +0530

    Create classifieds.component.ts

---
+++ src/app/modules/resident/components/classifieds/classifieds.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Classified } from '../../../core/models/classified.model';
+import * as AppActions from '../../../core/store/actions';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-classifieds',
+  templateUrl: './classifieds.component.html',
+  styleUrls: ['./classifieds.component.scss']
+})
+export class ClassifiedsComponent implements OnInit {
+  classifiedForm: FormGroup;
+  classifieds$: Observable<Classified[]>;
+  currentUserId = 'currentUserId';
+
+  constructor(private store: Store<{ app: AppState }>, private fb: FormBuilder) {
+    this.classifieds$ = store.select(state => state.app.classifieds);
+    this.classifiedForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.store.dispatch(AppActions.loadClassifieds());
+  }
+
+  submitClassified() {
+    if (this.classifiedForm.valid) {
+      const classified: Classified = {
+        id: Math.random().toString(),
+        userId: this.currentUserId,
+        ...this.classifiedForm.value,
+        status: 'pending'
+      };
+      this.store.dispatch(AppActions.addClassified({ classified }));
+      this.classifiedForm.reset();
+    }
+  }
+
+  retractClassified(id: string) {
+    this.store.dispatch(AppActions.retractClassified({ id }));
+  }
+}

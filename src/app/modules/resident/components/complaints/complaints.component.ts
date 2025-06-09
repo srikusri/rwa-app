edbb18f2e79e1bd67e32214b11242ede3152d433
Srinivas R <srinivaspes@gmail.com>
2025-06-09 15:44:00 +0530

    Create complaints.component.ts

---
+++ src/app/modules/resident/components/complaints/complaints.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Complaint } from '../../../core/models/complaint.model';
+import * as AppActions from '../../../core/store/actions';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-complaints',
+  templateUrl: './complaints.component.html',
+  styleUrls: ['./complaints.component.scss']
+})
+export class ComplaintsComponent implements OnInit {
+  complaintForm: FormGroup;
+  complaints$: Observable<Complaint[]>;
+
+  constructor(private store: Store<{ app: AppState }>, private fb: FormBuilder) {
+    this.complaints$ = store.select(state => state.app.complaints);
+    this.complaintForm = this.fb.group({
+      description: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.store.dispatch(AppActions.loadComplaints());
+  }
+
+  submitComplaint() {
+    if (this.complaintForm.valid) {
+      const complaint: Complaint = {
+        id: Math.random().toString(),
+        userId: 'currentUserId',
+        description: this.complaintForm.value.description,
+        status: 'open',
+        createdAt: new Date()
+      };
+      this.store.dispatch(AppActions.addComplaint({ complaint }));
+      this.complaintForm.reset();
+    }
+  }
+}
